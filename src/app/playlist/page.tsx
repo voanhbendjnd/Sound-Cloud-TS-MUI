33313ccaa2d59db62de14d749ddf7a641ca0cdeb
VoAnhBen <benva.ce190709@gmail.com>
2026-01-04 00:58:48 +0700

    update

---
+++ src/app/playlist/page.tsx
@@ -0,0 +1,11 @@
+import PrimarySearchAppBar from "@/components/headers/app.header";
+
+const PlaylistPage = () => {
+    return (
+        <div>
+            <PrimarySearchAppBar />
+            Playlist page
+        </div>
+    )
+}
+export default PlaylistPage;
