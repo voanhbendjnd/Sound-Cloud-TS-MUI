b0b79bdf5d1128173c13e4810b79a4de393ac9d2
VoAnhBen <benva.ce190709@gmail.com>
2026-02-15 16:11:08 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import MainSlider from '@/components/main/main.slider';
+import { Container } from '@mui/material';
 import * as React from 'react';
 
 export default function HomePage() {
   return (
     <div>
-      Home
+      <Container>
+        <MainSlider />
+
+      </Container>
     </div>
   )
 }
---
+++ src/components/main/main.slider.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React from "react";
+import Slider from "react-slick";
+import { Settings } from "react-slick"
+const MainSlider = () => {
+    const settings: Settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+    };
+    return (
+        <div>
+            <Slider {...settings}>
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3>4</h3>
+                </div>
+                <div>
+                    <h3>5</h3>
+                </div>
+                <div>
+                    <h3>6</h3>
+                </div>
+            </Slider>
+        </div>
+
+    );
+
+}
+
+export default MainSlider
