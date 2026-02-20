cb7b3cee89baf0e71ce12fb25848d280b8828d55
VoAnhBen <benva.ce190709@gmail.com>
2026-02-20 19:25:32 +0700

    Fetch Data HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,23 @@ import MainSlider from '@/components/main/main.slider';
 import { Container } from '@mui/material';
 import * as React from 'react';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await fetch("http://localhost:8080/api/v1/tracks", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application.json",
+    },
+    // body: JSON.stringify({
+    //   // category: "POP",
+    //   page: 1,
+    //   size: 5
+    // })
+  })
+  console.log("Check data server: ", await res.json());
   return (
     <div>
       <Container>
         <MainSlider />
-
       </Container>
     </div>
   )
