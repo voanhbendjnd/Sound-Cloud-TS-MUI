4d50716e472bc9b8f2d63e0811df7bf4cbfde725
VoAnhBen <benva.ce190709@gmail.com>
2026-04-09 00:09:36 +0700

    DJND config wavesurfer success

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,6 +1,21 @@
+'use client'
+import { useEffect } from "react";
+import WaveSurfer from "wavesurfer.js";
 const WaveTrack = () => {
+    useEffect(() => {
+        const element = document.getElementById("djnd");
+        if (element) {
+            WaveSurfer.create({
+                container: document.getElementById("djnd"),
+                waveColor: 'rgb(200, 0, 200)',
+                progressColor: 'rgb(100, 0, 100)',
+                url: '/audio/1771586892973-ytmp3free.cc_eleven-youtubemp3free.org.mp3',
+            })
+        }
+
+    }, [])
     return (
-        <div>
+        <div id="djnd">
             WakeTrack
         </div>
     )
